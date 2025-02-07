dbc610c1b9de562d28704843f8c3a2691cffe71b
TangoCh4rlie <elouanreymond@tutanota.com>
2025-02-07 14:46:36 +0100

    fix: get image

--- src/routes/content.route.ts
+++ src/routes/content.route.ts
@@ -1,63 +1,71 @@
-import { Context, Hono, Next } from "hono";
-import { jwt } from "hono/jwt";
+import { Context, Hono, Next } from 'hono';
+import { jwt } from 'hono/jwt';
 
 type Bindings = {
-  R2_BUCKET: R2Bucket;
+	R2_BUCKET: R2Bucket;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.get("/:fileName", async (c) => {
-  const fileName = c.req.param("fileName");
-  if (!fileName) {
-    return c.json("Query param 'name' is required", 400);
-  }
-  const content = await c.env.R2_BUCKET.get(fileName);
-  if (!content) {
-    return c.json(`${fileName} not found`, 404);
-  }
+app.get('/:fileName', async (c) => {
+	const fileName = c.req.param('fileName');
+	if (!fileName) {
+		return c.json("Query param 'name' is required", 400);
+	}
+	const object = await c.env.R2_BUCKET.get(fileName);
+	if (!object) {
+		return c.json(`${fileName} not found`, 404);
+	}
 
-  return c.body(content?.body);
+	const headers = new Headers();
+	object.writeHttpMetadata(headers);
+	headers.set('etag', object.httpEtag);
+
+	return new Response(object.body, {
+		headers,
+	});
 });
 
 app.use('/edit/*', async (c: Context, next: Next) => {
-  try {
-    await jwt({
-      cookie: 'token',
-      secret: c.env.JWT_SECRET_KEY,
-    })(c, next);
-  } catch (error) {
-    return c.json({ error: 'Unauthorized' }, 403);
-  }
+	try {
+		await jwt({
+			cookie: 'token',
+			secret: c.env.JWT_SECRET_KEY,
+		})(c, next);
+	} catch (error) {
+		return c.json({ error: 'Unauthorized' }, 403);
+	}
 });
 
-app.post("/edit", async (c) => {
-  const body = await c.req.parseBody();
-  const file = body["name"];
+app.post('/edit', async (c) => {
+	const body = await c.req.parseBody();
+	const file = body['content'];
 
-  if (!file || !(file instanceof File)) {
-    console.error("File uploaded is not a file");
-    return c.json("File uploaded is not a file", 400);
-  }
+	console.log(body['test']);
 
-  const r2Object = await c.env.R2_BUCKET.put(file.name, file);
-  if (!r2Object) {
-    console.error("Failed upload to R2", r2Object);
-    return c.json("Failed upload to R2", 500);
-  }
+	if (!file || !(file instanceof File)) {
+		console.error('File uploaded is not a file');
+		return c.json('File uploaded is not a file', 400);
+	}
 
-  return c.json("File uploaded successfully", 201);
+	const r2Object = await c.env.R2_BUCKET.put(file.name, file);
+	if (!r2Object) {
+		console.error('Failed upload to R2', r2Object);
+		return c.json('Failed upload to R2', 500);
+	}
+
+	return c.json('File uploaded successfully', 201);
 });
 
-app.delete("/edit/:fileName", async (c) => {
-  const fileName = c.req.param('fileName');
-  if (!fileName) {
-    return c.json("Query param 'name' is required", 400);
-  }
+app.delete('/edit/:fileName', async (c) => {
+	const fileName = c.req.param('fileName');
+	if (!fileName) {
+		return c.json("Query param 'name' is required", 400);
+	}
 
-  c.env.R2_BUCKET.delete(fileName)
+	c.env.R2_BUCKET.delete(fileName);
 
-  return c.json("File deleted with success", 200)
-})
+	return c.json('File deleted with success', 200);
+});
 
 export default app;
